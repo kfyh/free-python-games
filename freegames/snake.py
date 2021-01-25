4269bb45b348ed0a48936b878f44f7bb167546b6
Kevin Hew <kevinhew@gmail.com>
2021-01-25 09:44:38 +0100

    My snake changes

--- freegames/snake.py
+++ freegames/snake.py
@@ -14,11 +14,19 @@ from random import randrange
 from freegames import square, vector
 
 food = vector(0, 0)
-snake = [vector(10, 0)]
+snake = [vector(0, 100)]
 aim = vector(0, -10)
+
+growCount = 0;
+timerDelay = 200
 
 def change(x, y):
     "Change snake direction."
+    if ((x > 0 and aim.x < 0) or
+        (x < 0 and aim.x > 0) or
+        (y > 0 and aim.y < 0) or
+        (y < 0 and aim.y > 0)):
+        return
     aim.x = x
     aim.y = y
 
@@ -27,11 +35,24 @@ def inside(head):
     return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
+    global timerDelay
+    global growCount
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
 
-    if not inside(head) or head in snake:
+    if not inside(head):
+        if head.x >= 200:
+            head.x = -190
+        elif head.x <= -200:
+            head.x = 190
+
+        if head.y <= -200:
+            head.y = 190
+        elif head.y >= 200:
+            head.y = -190
+
+    if head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
@@ -40,21 +61,30 @@ def move():
 
     if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
-    else:
+        growCount = 2
+        if timerDelay > 50:
+            timerDelay -= 10
+        while True:
+            food.x = randrange(-15, 15) * 10
+            food.y = randrange(-15, 15) * 10
+            if food not in snake and food != head:
+                break
+
+    if growCount <= 0:
         snake.pop(0)
+    else:
+        growCount -= 1
 
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, 'blue')
 
     square(food.x, food.y, 9, 'green')
     update()
-    ontimer(move, 100)
+    ontimer(move, timerDelay)
 
-setup(420, 420, 370, 0)
+setup(420, 420, 0, 0)
 hideturtle()
 tracer(False)
 listen()
